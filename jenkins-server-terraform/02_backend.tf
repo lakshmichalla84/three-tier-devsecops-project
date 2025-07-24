a3b655af369fc84b0141c547e15a0b4f1ccd137f
lakshmichalla84 <136180406+lakshmichalla84@users.noreply.github.com>
2025-07-24 10:24:54 +0530

    Update 02_backend.tf

--- jenkins-server-terraform/02_backend.tf
+++ jenkins-server-terraform/02_backend.tf
@@ -2,13 +2,13 @@
 terraform {
   backend "s3" {
     # The S3 bucket where the Terraform state file will be stored.
-    bucket         = "0788-tier-devsecops-project-bucket-s3"
+    bucket         = "four-tier-devsecops-project-bucket-s3"
     
     # The AWS region where the S3 bucket and DynamoDB table are located.
     region         = "us-west-2"
     
     # The key (path) within the S3 bucket for storing the state file.
-    key            = "0788-tier-devdecops-project/jenkins-server-terraform/terraform.tfstate"
+    key            = "four-tier-devdecops-project/jenkins-server-terraform/terraform.tfstate"
     
     # The DynamoDB table used for state locking to prevent concurrent state changes.
     dynamodb_table = "lock-files"
